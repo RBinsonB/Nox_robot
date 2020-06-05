d7c528e78952625da7269b7a0c0fdce362763913
RobinB <robin-baran@hotmail.fr>
2020-06-05 09:13:45 +0200

    Fixed typo

--- Arduino/motor_controller_v2_AFMS/motor_controller_v2_AFMS.ino
+++ Arduino/motor_controller_v2_AFMS/motor_controller_v2_AFMS.ino
@@ -28,9 +28,9 @@ unsigned long lastMilli = 0;
 //--- Robot-specific constants ---
 const double radius = 0.04;                   //Wheel radius, in m
 const double wheelbase = 0.187;               //Wheelbase, in m
-const double encoder_cpr = 990                //Encoder ticks or counts per rotation
-const double speed_to_pwm_ratio = 0.00235     //Ratio to convert speed (in m/s) to PWM value. It was obtained by plotting the wheel speed in relation to the PWM motor command (the value is the slope of the linear function).
-const double min_speed_cmd = 0.0882           //(min_speed_cmd/speed_to_pwm_ratio) is the minimum command value needed for the motor to start moving. This value was obtained by plotting the wheel speed in relation to the PWM motor command (the value is the constant of the linear function).
+const double encoder_cpr = 990;               //Encoder ticks or counts per rotation
+const double speed_to_pwm_ratio = 0.00235;    //Ratio to convert speed (in m/s) to PWM value. It was obtained by plotting the wheel speed in relation to the PWM motor command (the value is the slope of the linear function).
+const double min_speed_cmd = 0.0882;          //(min_speed_cmd/speed_to_pwm_ratio) is the minimum command value needed for the motor to start moving. This value was obtained by plotting the wheel speed in relation to the PWM motor command (the value is the constant of the linear function).
 
 double speed_req = 0;                         //Desired linear speed for the robot, in m/s
 double angular_speed_req = 0;                 //Desired angular speed for the robot, in rad/s
